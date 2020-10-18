6544e2990a23127d06992e1f3ccee1f0f62cba58
Ricky Romero <ricky.romero@gmail.com>
2020-10-18 16:39:57 -0700

    Initial commit

---
+++ backend/src/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+app.get('/bye', (req, res) => {
+  res.send('Bye... World.')
+})
+
+app.listen(3000)
